601a8e69e49a0bbd905e08b0d5954a1457afb9aa
jbarretodev <jbarretodev6105@gmail.com>
2022-11-15 17:40:51 -0400

    https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,6 +30,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
         
         view()->composer('*', function ($view) {
             if (UtilityRepository::isSiteInstalled()==true) {
